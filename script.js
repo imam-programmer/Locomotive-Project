d4c0118b16f31c0d8ae0d0fc975b998c2df6a45a
imam-programmer <imamprogrammer09@gmail.com>
2026-06-15 20:25:42 +0600

    loading animation done

---
+++ script.js
@@ -0,0 +1,21 @@
+let tl=gsap.timeline()
+tl.to("#yellow1",{
+y:"-100%",
+duration:2,
+ease: "expo.out",
+})
+tl.from("#yellow2",{
+    y:"100%",
+ease: "expo.out",
+    duration:2,
+    delay:-1,
+
+},"enem")
+tl.to("#loader h1",{
+    delay:-1,
+    color:"black"
+},"enem")
+
+tl.to("#loader",{
+   display:"none"
+})
