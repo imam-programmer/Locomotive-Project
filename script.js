8a22e239fe08c72d27ef926e2eb6ed8c05b6f5dc
imam-programmer <imamprogrammer09@gmail.com>
2026-06-15 20:39:19 +0600

    locomotive js adding done

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-let tl=gsap.timeline()
-tl.to("#yellow1",{
-y:"-100%",
-duration:2,
-ease: "expo.out",
-})
-tl.from("#yellow2",{
-    y:"100%",
-ease: "expo.out",
+function Loading(){
+
+    let tl=gsap.timeline()
+    tl.to("#yellow1",{
+    y:"-100%",
     duration:2,
-    delay:-1,
+    ease: "expo.out",
+    })
+    tl.from("#yellow2",{
+        y:"100%",
+    ease: "expo.out",
+        duration:2,
+        delay:-1,
+    
+    },"enem")
+    tl.to("#loader h1",{
+        delay:-1,
+        color:"black"
+    },"enem")
+    
+    tl.to("#loader",{
+       display:"none"
+    })
+}
+Loading()
+const scroll = new LocomotiveScroll({
+    el:document.querySelector("#main"),
+    smooth:true
+});
 
-},"enem")
-tl.to("#loader h1",{
-    delay:-1,
-    color:"black"
-},"enem")
-
-tl.to("#loader",{
-   display:"none"
-})
