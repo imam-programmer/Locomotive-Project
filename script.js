71817ea8e03af2f140ff650f89583ec9c682ecc3
imam-programmer <imamprogrammer09@gmail.com>
2026-07-20 23:09:25 +0600

    some issue solve

--- script.js
+++ script.js
@@ -1,41 +1,46 @@
-function Loading(){
-    let tl=gsap.timeline()
-    tl.to("#yellow1",{
-    y:"-100%",
-    duration:2,
+function Loading() {
+  let tl = gsap.timeline();
+  tl.to("#yellow1", {
+    y: "-100%",
+    duration: 2,
     ease: "expo.out",
-    })
-    tl.from("#yellow2",{
-        y:"100%",
-    ease: "expo.out",
-        duration:2,
-        delay:-1,
-    
-    },"enem")
-    tl.to("#loader h1",{
-        delay:-1,
-        color:"black"
-    },"enem")
-    
-    tl.to("#loader",{
-       display:"none",
+  });
+  tl.from(
+    "#yellow2",
+    {
+      y: "100%",
+      ease: "expo.out",
+      duration: 2,
+      delay: -1,
+    },
+    "enem",
+  );
+  tl.to(
+    "#loader h1",
+    {
+      delay: -1,
+      color: "black",
+    },
+    "enem",
+  );
 
-    })
+  tl.to("#loader", {
+    display: "none",
+  });
 }
-Loading()
+Loading();
 const scroll = new LocomotiveScroll({
-    el:document.querySelector("#main"),
-    smooth:true
+  el: document.querySelector("#main"),
+  smooth: true,
 });
-document.querySelector("#footer h3").addEventListener("click",()=>{
-    scroll.scrollTo(0)
-})
-let elems=document.querySelectorAll(".elem")
-let main=document.querySelector("#page2")
-elems.forEach((ele)=>{
-    ele.addEventListener("mouseenter",()=>{
-       let bgimg=ele.getAttribute("data-img")
-       main.style.backgroundImage=`url(${bgimg})`
-    })
- 
-})
+document.querySelector("#footer h3").addEventListener("click", () => {
+  scroll.scrollTo(0);
+});
+let elems = document.querySelectorAll(".elem");
+let main = document.querySelector("#page2");
+elems.forEach((ele) => {
+  ele.addEventListener("mouseenter", () => {
+    let bgimg = ele.getAttribute("data-img");
+    main.style.backgroundImage = `url(${bgimg})`;
+  });
+});
